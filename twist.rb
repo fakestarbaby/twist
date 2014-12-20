d6d9da4831055c340d28056798979d12868a4f9e
sanemat <o.gata.ken@gmail.com>
2014-12-20 20:04:40 +0900

    Use httparty

--- twist.rb
+++ twist.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 require 'em-twitter'
 require 'json'
+require 'httparty'
 
 Bundler.require
 
@@ -18,14 +19,23 @@ options = {
   }
 }
 
+hubot_endpoint = ENV['HUBOT_ENDPOINT']
+hubot_room = ENV['HUBOT_ROOM']
+
 EM.run do
   twitter_client = EM::Twitter::Client.connect(options)
 
-  twitter_client.each do |result|
-    result = JSON.parse(result)
+  twitter_client.each do |matched|
+    result = JSON.parse(matched)
     next if ignore_users.include?(result['user']['screen_name'])
     next if track_keywords.include?(result['user']['screen_name'])
 
-    status_url = "https://twitter.com/#{result['user']['screen_name']}/status/#{result['id']}"
+    user_image = result['user']['profile_image_url_https']
+    user_screen_name = result['user']['screen_name']
+    status = result['text']
+    status_url = "https://twitter.com/#{result['user']['screen_name']}/status/#{result['id_str']}"
+    message = [user_image, user_screen_name, status, status_url].join("\n")
+
+    HTTParty.post(hubot_endpoint, {room: hubot_room, message: message})
   end
 end
